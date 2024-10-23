18dfd17668142e29560d76a17d487d28c45c5641
jonahng <nyhj1998@gmail.com>
2024-10-23 13:36:26 +0800

    finished consoleInputTemplate

--- src/ConsoleInputTemplate.java
+++ src/ConsoleInputTemplate.java
@@ -3,13 +3,15 @@ import java.io.Console;
 public class ConsoleInputTemplate{
 
     public static void main(String[] args) {
+        System.out.println("App running!");
         
         String userInput = "";
 
-        while(userInput.equals("quit")){
+        while(!userInput.equals("quit")){
 
             Console console = System.console();
             userInput = console.readLine("Type in your input/guess");
+            System.out.println("You typed: " + userInput);
 
         }
     }
